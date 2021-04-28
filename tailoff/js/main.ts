65d874e7abd2a158a9e6cb83a498f5574c962a8a
Caro03 <caroverstreken@hotmail.com>
2021-04-28 12:04:30 +0200

    Added pagination to index

--- tailoff/js/main.ts
+++ tailoff/js/main.ts
@@ -48,8 +48,8 @@ new formOtherRadioComponent();
 import { GlideComponent } from "./components/glide.component";
 new GlideComponent();
 
-import { GoogleMapsComponent } from "./components/googleMaps.component";
-new GoogleMapsComponent();
+//import { GoogleMapsComponent } from "./components/googleMaps.component";
+//new GoogleMapsComponent();
 
 import { IndeterminateChecksComponent } from "./components/indeterminateChecks.component";
 new IndeterminateChecksComponent();
